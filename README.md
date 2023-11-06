5acedecd25b0baed7a83e226e3c55adab796a29c
Bruno Fauth <149593@upf.br>
2023-11-05 23:44:38 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,231 @@
+# Introduction
+
+`funparse` allows you to "derive" an argument parser (such as those from 
+[argparse](https://docs.python.org/3/library/argparse.html)) from type 
+annotations of a function's signature, cutting down on the boilerplate code. 
+It's similar to [fire](https://github.com/google/python-fire) in this way, but 
+it's more lightweight and designed in a way to give its user more control over 
+what is going on.
+
+
+**Disclaimer:** your user experience may be much richer if you're using a 
+static type checker, such as [mypy](https://mypy-lang.org/)
+
+
+# Installation
+
+With pip:
+
+    pip install funparse
+
+With poetry:
+    
+    poetry install funparse
+
+
+# Examples
+
+## Basic Usage
+
+```python
+import sys
+import funparse.api as fp
+
+@fp.as_arg_parser
+def some_parser_name(
+    your_name: str,
+    your_age: int,
+    pets: list[str] | None = None,
+    loves_python: bool = False,
+) -> None:
+    print("Hi", your_name)
+
+    if pets is not None:
+        for pet in pets:
+            print("send greetings to", pet, "for me")
+
+    if loves_python:
+        print("Cool! I love python too!")
+
+
+# Run the parser on this set of arguments
+some_parser_name.run([
+    "Johnny",
+    "33",
+    "--pets", "Goofy",
+    "--pets", "Larry",
+    "--pets", "Yes",
+    "--loves-python",
+])
+
+# You can also use args from stdin
+some_parser_name.run(sys.stdin[1:])
+```
+
+## Printing Help
+
+```python
+import funparse.api as fp
+
+@fp.as_arg_parser
+def some_parser_name(
+    your_name: str,
+    your_age: int,
+) -> None:
+    print("Hi", your_name)
+    if your_age > 325:
+        print("getting elderly, eh")
+
+# You can print help and usage information like this:
+some_parser_name.print_usage()
+some_parser_name.print_help()
+# These work just like they do on 'argparse.ArgumentParser'
+
+# You can also format this information into strings
+usage = some_parser_name.format_usage()
+help_str = some_parser_name.format_help()
+```
+
+[See more about it 
+here](https://docs.python.org/3/library/argparse.html#printing-help)
+
+
+## Behavior on Booleans
+
+```python
+import funparse.api as fp
+
+@fp.as_arg_parser
+def booler(
+    aaa: bool, # This is a positional argument
+    bbb: bool = True, # This is a flag which, if present, will set 'bbb' to False
+    ccc: bool = False, # This is a flag which, if set, will set 'ccc' to True
+) -> None:
+    print(aaa, bbb, ccc)
+
+# This will print: True, False, False
+booler.run([
+    "yes", # 'y', 'true', 'True' and '1' will also work
+    "--bbb",
+])
+
+# This will print: False, True, False
+booler.run([
+    "false", # 'n', 'no', 'False' and '0' will also work
+])
+```
+
+
+## Behavior on Enums
+
+```python
+import funparse.api as fp
+import enum
+
+
+# This Enum functionality will work better if you user SCREAMING_SNAKE_CASE for 
+# the names of your enum members (if you don't, your CLI will work in a
+# case-sensitive way :P)
+class CommandModes(fp.Enum): # You can use enum.Enum and similar classes too
+    CREATE_USER = enum.auto()
+    LIST_USERS = enum.auto()
+    DELETE_USER = enum.auto()
+
+
+@fp.as_arg_parser
+def some_parser(mode: CommandModes) -> None:
+    print(f"you picked {mode.name!r} mode!")
+
+
+some_parser.run(["CREATE_USER"]) # This is valid...
+some_parser.run(["create_user"]) # ...so is this...
+some_parser.run(["crEatE_usEr"]) # ...and this too...
+
+# This raises an error
+some_parser.run(["NON_EXISTING_FUNCTIONALITY"])
+```
+
+## Bypassing the command-line
+
+If you want to pass extra data to the function which you're using as your 
+parser generator, but without having to supply this data through the CLI, you 
+can use the `ignore` parameter on `as_arg_parser`, like this:
+
+```python
+import funparse.api as fp
+
+
+@fp.as_arg_parser(ignore=["user_count", "user_name"])
+def some_parser(
+    user_count: int,
+    user_name: str,
+    user_address: str
+    is_foreigner: bool = False,
+) -> None:
+    print(f"you're the {user_count}th user today! welcome, {user_name}")
+    print("They say", user_address, "is lovely this time of the year...")
+
+
+# These 'state-variables' must be passed as keyword args (or through **kwargs)
+some_parser.with_state(
+    user_count=33,
+    user_name="Josh",
+).run(["some address..."])
+
+# If you want, you can cache these parser-with-state objects. It sort of
+# reminds me of 'functools.partial'
+saving_for_later = some_parser.with_state(
+    user_count=33,
+    user_name="Josh",
+)
+
+# Later:
+saving_for_later.run([
+    "some address...",
+    "--is-foreigner"
+])
+```
+
+
+## Using custom argument parsers
+
+```python
+import argparse
+import funparse.api as fp
+
+
+# First, subclass 'argparse.ArgumentParser'
+class MyParser(argparse.ArgumentParser):
+    """Just like argparse's, but better!"""
+
+
+# Then, pass your parser as an argument to 'as_arg_parser'
+@fp.as_arg_parser(parser_type=MyParser)
+def some_parser(
+    user_name: str,
+    is_foreigner: bool = False,
+) -> None:
+    print("Welcome", user_name)
+    if is_foreigner:
+        print("Nice to have you here")
+
+
+# Finally, run your parser. It all works as expected!
+some_parser.run([
+    "johnny",
+    "--is-foreigner",
+])
+```
+
+
+## Extras
+
+Beyond `as_arg_parser`, this module also ships:
+
+- `funparse.Enum`, which is a subclass of `enum.Enum`, but with a `__str__` 
+  that better fits your CLI apps
+- `funparse.ArgumentParser`, which is a subclass of `argparse.ArgumentParser` 
+  that, unlike the latter, does not terminate your app on (most) exceptions
+
+Have fun!
+
---
+++ sigparse/api.py
@@ -0,0 +1,237 @@
+from __future__ import annotations
+
+import contextlib
+from collections.abc import Callable, Sequence
+from inspect import Parameter
+from types import GenericAlias, UnionType
+from typing import ParamSpec, TypeVar, Self, Generic, Any
+import enum
+import argparse as ap
+import functools
+import inspect
+import builtins
+
+P = ParamSpec("P")
+T = TypeVar("T")
+U = TypeVar("U")
+Constructor = Callable[[T], U]
+
+__all__ = [
+    "ArgumentParser",
+    "as_arg_parser",
+    "Command",
+    "Enum",
+]
+
+
+class ArgumentParser(ap.ArgumentParser):
+    """Wrapper around 'argparse.ArgumentParser' in which 'self.error' \
+       doesn't call 'sys.exit'"""
+
+    def error(self, message: str):
+        """error(message: string)
+
+        Prints a usage message incorporating the message to stderr and
+        exits.
+
+        If you override this in a subclass, it should not return -- it
+        should either exit or raise an exception.
+        """
+        raise RuntimeError(f'{self.prog}: error: {message}')
+
+
+def make_argument_name(base_name: str, optional: bool) -> str:
+    strikethrough = base_name.replace("_", "-")
+    return "--" + strikethrough if optional else strikethrough
+
+
+def johnny_simple(
+    in_type: type,
+    default_val: Any,
+) -> tuple[str, Constructor, list[str] | None]:
+    match (in_type, default_val):
+        case (builtins.bool, True):
+            return "store_false", bool, None
+        case (builtins.bool, False):
+            return "store_true", bool, None
+        case (builtins.bool, Parameter.empty):
+            return "store", _bool_from_str, None
+        case (enum.EnumType() as enum_type, _):
+            return (
+                "store",
+                functools.partial(_enum_from_str, enum_type),
+                list(enum_type),
+            )
+        case (some_type, _) if some_type in (int, float, str):
+            return "store", some_type, None
+        case _:
+            raise RuntimeError(f"unsupported type: {in_type!r}")
+    return ()  # type: ignore
+
+
+def johnny_generic(
+    in_type: GenericAlias,
+    default_val: Any,
+) -> tuple[str, type, list[str] | None]:
+    match (in_type.__origin__, in_type.__args__, default_val):
+        case (seq, [single_type], _) if issubclass(seq, Sequence):
+            return "append", single_type, None
+        case _:
+            raise RuntimeError(f"unsupported generic type: {in_type!r}")
+    return ()  # type: ignore
+
+
+def _bool_from_str(word: str) -> bool:
+    match word.lower():
+        case "y" | "yes" | "true" | "1":
+            return True
+        case "n" | "no" | "false" | "0":
+            return False
+        case _:
+            raise ValueError(f"invalid value for boolean: {word!r}")
+
+
+def _enum_from_str(enum_type: type[enum.Enum], word: str) -> enum.Enum:
+    with contextlib.suppress(KeyError):
+        return enum_type[word]
+    with contextlib.suppress(KeyError):
+        return enum_type[word.upper()]
+    raise ValueError(f"no name {word!r} in {enum_type!r}")
+
+
+def _unwrap_optional_type(opt: UnionType) -> type:
+    return opt.__args__[opt.__args__.index(type(None)) - 1]
+
+
+def make_parser(
+    fn: Callable[P, T],
+    ignore: Sequence[str] | None = None,
+    parser_type: type[ap.ArgumentParser] = ArgumentParser,
+) -> ap.ArgumentParser:
+    parser = parser_type(
+        description=fn.__doc__,
+        exit_on_error=False,
+    )
+    signature = inspect.signature(fn)
+
+    for name, body in signature.parameters.items():
+        if ignore is not None and name in ignore:
+            continue
+        raw_type = body.annotation
+        default = body.default
+
+        if isinstance(raw_type, UnionType):
+            if len(raw_type.__args__) != 2 or type(
+                    None) not in raw_type.__args__:
+                raise TypeError(f"unsupported union: {raw_type!r}")
+            raw_type = _unwrap_optional_type(raw_type)
+            if default is Parameter.empty:
+                default = None
+
+        match raw_type:
+            case Parameter.empty:
+                raise SyntaxError(
+                    f"untyped parameters are not supported: {name!r}")
+            case type():
+                action, _type, choices = johnny_simple(raw_type, default)
+            case GenericAlias():
+                action, _type, choices = johnny_generic(raw_type, default)
+
+        add_argument_params = {}
+        if default is not Parameter.empty:
+            add_argument_params["default"] = default
+        match action:
+            case "append":
+                add_argument_params["choices"] = choices
+                add_argument_params["type"] = _type
+            case "store":
+                add_argument_params["choices"] = choices
+                add_argument_params["type"] = _type
+            case "store_true" | "store_false":
+                pass
+            case other:
+                raise ValueError(f"unsupported action: {other!r}")
+
+        parser.add_argument(
+            make_argument_name(name, default is not Parameter.empty),
+            action=action,
+            **add_argument_params,
+        )
+
+    return parser
+
+
+def as_arg_parser(
+    fn: Callable[P, T] | None = None,
+    ignore: Sequence[str] | None = None,
+    parser_type: type[ap.ArgumentParser] = ArgumentParser,
+):
+    if fn is not None:
+        return as_arg_parser_inner(
+            fn=fn,
+            ignore=ignore,
+            parser_type=parser_type,
+        )
+
+    return functools.partial(
+        as_arg_parser_inner,
+        ignore=ignore,
+        parser_type=parser_type,
+    )
+
+
+def as_arg_parser_inner(
+    fn: Callable[P, T],
+    ignore: Sequence[str] | None = None,
+    parser_type: type[ap.ArgumentParser] = ArgumentParser,
+) -> Command[P, T]:
+    return Command(
+        fn=fn,
+        parser=make_parser(fn=fn, ignore=ignore, parser_type=parser_type),
+    )
+
+
+class Command(Generic[P, T]):
+    __slots__ = [
+        "_state",
+        "_fn",
+        "_parser",
+        "print_usage",
+        "print_help",
+        "format_usage",
+        "format_help",
+    ]
+
+    def __init__(
+        self,
+        fn: Callable[P, T],
+        parser: ap.ArgumentParser,
+        state: dict[str, Any] | None = None,
+    ) -> None:
+        self._fn = fn
+        self._parser = parser
+        self._state = state or {}
+        self.print_usage = parser.print_usage
+        self.print_help = parser.print_help
+        self.format_usage = parser.format_usage
+        self.format_help = parser.format_help
+
+    def with_state(self: Self, **kwargs) -> Command[P, T]:
+        self._state = kwargs
+        return Command(fn=self._fn, parser=self._parser, state=kwargs)
+
+    def run(self: Self, cmd_args: list[str]) -> T:
+        return self._fn(
+            **self._state,
+            **vars(self._parser.parse_args(cmd_args)),
+        )
+
+    def show_help(self) -> None:
+        self.run(["--help"])
+
+
+class Enum(enum.Enum):
+    """Subclass of 'enum.Enum' with a CLI-friendly '__str__'"""
+
+    def __str__(self) -> str:
+        return self.name
